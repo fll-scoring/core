0b086466fd6d7b798ecc733b7abb0f07268ace14
Owen Salter <owen@devosmium.xyz>
2020-08-01 16:23:46 -0400

    feat: Move to postgresql

--- src/data.rs
+++ src/data.rs
@@ -1,55 +1,17 @@
 use super::config::get_global_value;
-use bson::doc;
-use mongodb::{options::ClientOptions, Client, Collection, Database};
-use std::env;
+use sqlx::postgres::PgPool;
+use super::errors::ServiceError;
 
-/// Returns a mongo client according to the configured mongo url
-pub async fn get_mongo_client() -> Result<Client, Box<dyn std::error::Error>> {
-    let mongo_url = get_global_value("mongo-url", true)?;
-    let client_options = ClientOptions::parse(&mongo_url).await?;
+pub async fn get_db_pool() -> Result<PgPool, super::errors::ServiceError> {
+  let db_url = get_global_value("postgres-url", true)?;
+  let pool = PgPool::builder()
+    .max_size(5u32)
+    .build(db_url.as_str()).await;
 
-    let client = Client::with_options(client_options)?;
-
-    Ok(client)
+  match pool {
+    Ok(pool) => Ok(pool),
+    Err(_) => Err(ServiceError::InternalServerError)
+  }
 }
 
-/// Returns either the Mongo database from the client returned by get_mongo_client() or the Error.
-pub async fn get_mongo_database() -> Result<Database, Box<dyn std::error::Error>> {
-    let client = get_mongo_client().await?;
 
-    let mongo_database = get_global_value("mongo-database", true)?;
-
-    let database = client.database(&mongo_database);
-
-    Ok(database)
-}
-
-/// Gets the next number in a mongo sequence
-pub async fn get_next_mongo_sequence_number(
-    name: String,
-    counter_collection: Collection,
-) -> Result<i32, Box<dyn std::error::Error>> {
-    let sequence_document = match counter_collection
-        .find_one_and_update(
-            doc! {"_id": name.clone()},
-            doc! {"$inc": {"sequence_value": 1}},
-            None,
-        )
-        .await
-    {
-        Ok(opt) => match opt {
-            Some(doc) => doc,
-            None => {
-                counter_collection
-                    .insert_one(doc! {"_id": name, "sequence_value": 0}, None)
-                    .await?;
-                doc! {"sequence_value": 0}
-            }
-        },
-        Err(err) => {
-            return Err(Box::new(err));
-        }
-    };
-    let sequence_value = sequence_document.get_i32("sequence_value")?;
-    Ok(sequence_value)
-}
